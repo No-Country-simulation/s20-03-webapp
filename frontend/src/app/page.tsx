f778363e0d6adf6db451d0cb828f50a6662e26f4
Eduardo <ejrb1234@gmail.com>
2025-01-10 21:14:17 -0400

    build(app): Añadiendo ReactHookForm

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,10 @@
+import { TestForm } from "./components/TestForm";
+
 export default function HomePage() {
-  return <h2 className="text-3xl font-bold underline">HomePage</h2>;
+  return (
+    <>
+      <h2 className="text-3xl font-bold underline">HomePage</h2>
+      <TestForm />
+    </>
+  );
 }
