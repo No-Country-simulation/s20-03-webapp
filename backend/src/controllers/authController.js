8a0b7edd8bf49ccc34fb61415c0b89be66c352d5
Gabriel Hernández <gabrielymarg@gmail.com>
2025-01-21 20:21:16 -0300

    roleMiddleware implemented, minor updates.

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,25 +4,30 @@ const responses = require('../utils/responses');
 
 const authController = {
     login: async (req, res) => {
-        const { username, password } = req.body;
-        const user = await userModel.findOne({ username });
-        if (!user) {
-            return res.status(responses.auth.unauthorized.status).json(responses.auth.unauthorized);
+        try {
+            const { username, password } = req.body;
+            const user = await userModel.findOne({ username });
+            if (!user) {
+                return res.status(responses.auth.unauthorized.status).json(responses.auth.unauthorized);
+            }
+            const match = await user.matchPassword(password);
+            if (!match) {
+                return res.status(responses.auth.unauthorized.status).json(responses.auth.unauthorized);
+            }
+            const token = jwt.sign({ id: user._id, role: user.role }, config.auth.secret, { expiresIn: config.auth.expiresIn });
+            payload = {
+                id: user._id,
+                fullname: user.fullname,
+                role: user.role
+            };
+            res.cookie('token', token, { httpOnly: true });
+            res.status(responses.common.payload().status).json(responses.common.payload(payload));
+        } catch (error) {
+            console.error(error);
+            res.status(responses.common.internalServerError).json(responses.common.internalServerError);
         }
-        const match = await user.matchPassword(password);
-        if (!match) {
-            return res.status(responses.auth.unauthorized.status).json(responses.auth.unauthorized);
-        }
-        const token = jwt.sign({ id: user._id, role: user.role }, config.auth.secret, { expiresIn: config.auth.expiresIn });
-        payload = {
-            id: user._id,
-            fullname: user.fullname,
-            role: user.role
-        };
-        res.cookie('token', token, { httpOnly: true });
-        res.status(responses.common.payload().status).json(responses.common.payload(payload));
     },
-    register: (req, res) => {
+    register: async (req, res) => {
         res.send('Register route');
     }
 }
--- backend/src/middleware/roleMiddleware.js
+++ backend/src/middleware/roleMiddleware.js
@@ -1,11 +1,32 @@
 const responses = require('../utils/responses');
 
 const roleMiddleware = async (req, res, next) => {
-    try {
-
-    } catch (error) {
-        
-    }
+    const route = req.url.split('/')[1];
+    const { role } = req.user;
+    switch (route) {
+        case 'manager':
+            if (role !== 'manager') {
+                return res.status(responses.auth.forbidden.status).json(responses.auth.forbidden);
+            };
+            return next();
+        case 'teacher':
+            if (role !== 'teacher') {
+                return res.status(responses.auth.forbidden.status).json(responses.auth.forbidden);
+            };
+            return next();
+        case 'student':
+            if (role !== 'student') {
+                return res.status(responses.auth.forbidden.status).json(responses.auth.forbidden);
+            };
+            return next();
+        case 'parent':
+            if (role !== 'parent') {
+                return res.status(responses.auth.forbidden.status).json(responses.auth.forbidden);
+            };
+            return next();
+        default:
+            return res.status(responses.common.notImplemented.status).json(responses.common.notImplemented);
+    };
 };
 
 module.exports = roleMiddleware;
