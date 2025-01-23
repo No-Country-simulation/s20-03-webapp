bdcb72c0afcfe2778d28833c50522f54596f59ed
M-Azariel <walter.azariel.moreno@gmail.com>
2025-01-23 19:08:40 -0300

    comentando lineas 9 a  32 de layout

--- frontend/src/app/(pages)/(dashboard)/layout.tsx
+++ frontend/src/app/(pages)/(dashboard)/layout.tsx
@@ -6,44 +6,44 @@ interface LayoutProps {
 }
 
 
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [status, setStatus] = useState("")
+//   const [username, setUsername] = useState("")
+//   const [password, setPassword] = useState("")
+//   const [status, setStatus] = useState("")
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    setStatus("loading")
+//   const handleSubmit = async (e) => {
+//     e.preventDefault()
+//     setStatus("loading")
 
-    try {
-      const response = await axios.post(
-        "https://p45c4l.ngcomputers.com.ar/auth/register ",
-        {
-          username,
-          password,
-        }
-      );
-      setStatus("success");
-      console.log("Registro exitoso:", response.data);
-    } catch (error) {
-      setStatus("error");
-      console.error("Error al registrar:", error.response || error.message);
-    }
-  };
+//     try {
+//       const response = await axios.post(
+//         "https://p45c4l.ngcomputers.com.ar/auth/register ",
+//         {
+//           username,
+//           password,
+//         }
+//       );
+//       setStatus("success");
+//       console.log("Registro exitoso:", response.data);
+//     } catch (error) {
+//       setStatus("error");
+//       console.error("Error al registrar:", error.response || error.message);
+//     }
+//   };
 
 export default function Layout({ children }: LayoutProps) {
 
-  return (
-    <>
-      <AppSidebar />
-      <div className="flex w-full flex-col">
-        <Header>
-          {`<Search/>`}
-          <div className="ml-auto flex items-center gap-4">
-            {`<ExampleComponente/>`}
-          </div>
-        </Header>
-        <main className="flex-1">{children}</main>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <AppSidebar />
+            <div className="flex w-full flex-col">
+                <Header>
+                    {`<Search/>`}
+                    <div className="ml-auto flex items-center gap-4">
+                        {`<ExampleComponente/>`}
+                    </div>
+                </Header>
+                <main className="flex-1">{children}</main>
+            </div>
+        </>
+    )
 }
