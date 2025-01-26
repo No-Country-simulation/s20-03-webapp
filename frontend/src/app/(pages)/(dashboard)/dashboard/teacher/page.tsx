d09fc78369acad3fbc7fd619de2d60f33468d2c5
M-Azariel <walter.azariel.moreno@gmail.com>
2025-01-25 21:26:12 -0300

    modificando flex por grid

--- frontend/src/app/(pages)/(dashboard)/dashboard/teacher/page.tsx
+++ frontend/src/app/(pages)/(dashboard)/dashboard/teacher/page.tsx
@@ -1,46 +1,53 @@
-"use client"
+'use client'
 
-import { UpdateGrades } from "#/src/app/components/molecules/card-grades";
-import { CardTeacher } from "#/src/app/components/molecules/card-teacher"
-import { ComboboxDemo } from "#/src/app/components/molecules/combobox";
-import { NotificationsTeacher } from "#/src/app/components/molecules/notifications-teacher";
-import { Users, Calculator } from 'lucide-react';
-import {Asistence} from '#/src/app/components/molecules/asistence'
+import { Calculator, Users } from 'lucide-react'
 
+import { Section } from '#/src/app/components/atoms/section'
+import { UpdateGrades } from '#/src/app/components/molecules/card-grades'
+import { CardTeacher } from '#/src/app/components/molecules/card-teacher'
+import { ComboboxDemo } from '#/src/app/components/molecules/combobox'
+import { NotificationsTeacher } from '#/src/app/components/molecules/notifications-teacher'
 
-export default function TeacherPage(){
+export default function TeacherPage() {
     return (
-        <div className="bg-[#bg-[#FCFCFC]] w-full">
-            <aside className="flex justify-between w-[69%] items-center mt-12 mb-14">
+        <Section className="w-full bg-[#FCFCFC]">
+            <aside className="mb-14 mt-12 flex w-[69%] items-center justify-between">
                 <h1 className="text-4xl">Bienvenido/a, Amanda martinez</h1>
                 <div className="mr-56">
-                    <ComboboxDemo/>
+                    <ComboboxDemo />
                 </div>
             </aside>
-            <section className="flex flex-col">
-                {/*3 cards total / promedio / tareas */}
-                <div className="flex gap-7">
-                    <CardTeacher title={"Total de estudiantes en la comision"} info={13} icon={<Users className=" w-4 h-4"/>}/>
-                    <CardTeacher title={"Promedio de la comision"} info={8.6} icon={<Calculator className=" w-4 h-4"/>}/>
-                    <CardTeacher title={"Tareas hechas"} info={<span>{10} / {13}</span> } icon={<Users className=" w-4 h-4"/>}/>
+            <section className="grid gap-10">
+                <div className="grid grid-cols-3 gap-7">
+                    <CardTeacher
+                        title={'Total de estudiantes en la comisión'}
+                        info={13}
+                        icon={<Users className="h-4 w-4" />}
+                    />
+                    <CardTeacher
+                        title={'Promedio de la comisión'}
+                        info={8.6}
+                        icon={<Calculator className="h-4 w-4" />}
+                    />
+                    <CardTeacher
+                        title={'Tareas hechas'}
+                        info={
+                            <span>
+                                {10} / {13}
+                            </span>
+                        }
+                        icon={<Users className="h-4 w-4" />}
+                    />
                 </div>
-                {/*segunda seccion con cards subir notas, notifications y asistencia */}
-                <div className="flex gap-7">
-                    <div className="flex flex-col mt-10 gap-10">
-                        <div>
-                            <UpdateGrades/>
-                        </div>
-                        <div>
-                            <NotificationsTeacher/>
-                        </div>
+                <div className="grid grid-cols-2">
+                    <div className="grid gap-10">
+                        <UpdateGrades />
+                        <NotificationsTeacher />
                     </div>
-                
-                    <div className="mt-10">
-                        <Asistence/>
-                    </div>
+                    
+                    <h1>otro contenido </h1>
                 </div>
             </section>
-        </div>
-        
+        </Section>
     )
 }
