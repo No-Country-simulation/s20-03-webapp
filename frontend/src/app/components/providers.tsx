ba97ec46bf4b69187088f78b0bea12d8896ca57d
Eduardo <ejrb1234@gmail.com>
2025-01-30 23:37:24 -0400

    feat(app): Se agregó el componente de <Toast/>

--- frontend/src/app/components/providers.tsx
+++ frontend/src/app/components/providers.tsx
@@ -1,10 +1,12 @@
 'use client'
 
-import { SidebarProvider } from './ui/sidebar'
+import { SidebarProvider } from '@/components/ui/sidebar'
+import { Toaster } from '@/components/ui/toaster'
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
+      <Toaster />
       <SidebarProvider>{children}</SidebarProvider>
     </>
   )
